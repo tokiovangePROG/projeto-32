cb061b9ac682a099c2454afa5d2f995614bd0096
otavio <107579773+tokiovangePROG@users.noreply.github.com>
2022-09-13 18:13:37 -0300

    Add files via upload

---
+++ Player.js
@@ -0,0 +1,45 @@
+class Player {
+  constructor(x, y, width, height) {
+    var options = {
+      isStatic: true
+    };
+
+    this.body = Bodies.rectangle(x, y, width, height, options);
+
+    this.width = width;
+    this.height = height;
+    this.image = loadImage("player.png");
+
+    this.life1 = "green";
+    this.life2 = "green";
+    this.life3 = "green";
+
+    World.add(world, this.body);
+  }
+
+  life() {
+    push();
+    textSize(20);
+    fill("white");
+    text("Player", 280, 40);
+
+    fill(this.life1);
+    rect(width-860,50,70,30);
+    rect(width-789,50,70,30);
+    rect(width-718,50,70,30);
+    // crie a vida em forma de retângulo
+    pop();
+  }
+
+ 
+  display() {
+    var pos = this.body.position;
+    var angle = this.body.angle;
+    push();
+    translate(pos.x, pos.y);
+    rotate(angle);
+    imageMode(CENTER);
+    image(this.image, 0, 0, this.width, this.height);
+    pop();
+  }
+}
